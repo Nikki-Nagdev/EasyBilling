853b99eee7ca4581aa57c42a170e52ef621cb7a1
Nikki-Nagdev <nagdevnikki@gmail.com>
2021-05-30 09:55:36 +0530

    Initial Commit

---
+++ src/main/java/com/easybilling/EasyBillingApplication.java
@@ -0,0 +1,13 @@
+package com.easybilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easybilling/EasyBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
